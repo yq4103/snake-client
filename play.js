c39c0bd1a354db0c5a1301888987cd87bf1a4c81
Yirong Qin <yq4103@mun.ca>
2021-08-26 00:26:44 +0000

    play with setupinput and handleuserinput

--- play.js
+++ play.js
@@ -3,3 +3,22 @@ const net = require("net");
 //connect function moved to client.js
 
 const { myConnection } = require('./client');
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput()
